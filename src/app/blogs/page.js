dfe0e2f638058f23a6e1fb68e04c0dc5927e4322
maythanhsuaung <maythanhsuaung1111@gmail.com>
2025-06-03 18:08:50 -0400

    changed data

--- src/app/blogs/page.js
+++ src/app/blogs/page.js
@@ -12,9 +12,9 @@ const BlogMainPage = async () => {
   return (
     <main className='min-h-screen bg-[#343646] relative'>
       <SideNav position="vertical" />
-      <div className='w-[85%] lg:w-[70%] m-auto  pt-[6em] lg:pt-[8em]'>
+      <div className='w-[90%] lg:w-[70%] m-auto  py-[6em] lg:py-[8em]'>
         <h2 className='text-3xl font-bold mb-10 text-white'>Blogs</h2>
-        <div className='grid gap-4 lg:grid-cols-3'>
+        <div className='grid gap-6 lg:grid-cols-3'>
           {result?.map((post) => (
             <BlogCard key={post.id} slug={post.slug} tags={post.tags} id={post.id} url={post.url} time={post.time} date={post.date} title={post.title} />
           ))}
